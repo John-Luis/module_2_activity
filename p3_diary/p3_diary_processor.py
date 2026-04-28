4a383c54e691c519f2c0a728efaf10d015c4d24e
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-04-29 01:00:40 +0800

    putted it into a def start_session for a more clear organized looking maangas code structure.

--- p3_diary/p3_diary_processor.py
+++ p3_diary/p3_diary_processor.py
@@ -2,10 +2,13 @@ import time
 import os
 from p3_diary_logic import LifeVault
 
-CONSOLE_BAR = "\033[97m\033[40m"
-CYAN, YELLOW, BOLD, RESET = "\033[96m", "\033[93m", "\033[1m", "\033[0m"
+def start_session():
+    CONSOLE_BAR = "\033[97m\033[40m"
+    CYAN, YELLOW, BOLD, RESET = "\033[96m", "\033[93m", "\033[1m", "\033[0m"
 
-manager = LifeVault("mylife.txt")
-os.system('cls' if os.name == 'nt' else 'clear')
+    manager = LifeVault("mylife.txt")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-print(f"{CONSOLE_BAR}{BOLD}  SYSTEM BOOT: {manager.today.upper()}  {RESET}\n")
+    print(f"{CONSOLE_BAR}{BOLD}  SYSTEM BOOT: {manager.today.upper()}  {RESET}\n")
+
+ 
