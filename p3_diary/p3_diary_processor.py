f5b42d9238b52d15dc380f50c1d1d1e6939bad74
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-04-29 00:58:07 +0800

    adding the manage variable, for easy access to the class module.. and we assigned it also for efficient accessing. Also added the feature where we us the library os, for clearing the visual terminal...

--- p3_diary/p3_diary_processor.py
+++ p3_diary/p3_diary_processor.py
@@ -4,3 +4,8 @@ from p3_diary_logic import LifeVault
 
 CONSOLE_BAR = "\033[97m\033[40m"
 CYAN, YELLOW, BOLD, RESET = "\033[96m", "\033[93m", "\033[1m", "\033[0m"
+
+manager = LifeVault("mylife.txt")
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print(f"{CONSOLE_BAR}{BOLD}  SYSTEM BOOT: {manager.today.upper()}  {RESET}\n")
