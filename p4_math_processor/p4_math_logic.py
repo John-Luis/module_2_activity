5ad914814fdccb4684cfe30111ae418179971efa
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-04-29 01:36:28 +0800

    created the logic for streaming the double and streaming text by its vaiable d_file and t_file. Now opening it, and using conditional statements on if its even... it will be powered. if else, it wil be go into t_file and be cubed. We did a manual opening and closing of txt file here.

--- p4_math_processor/p4_math_logic.py
+++ p4_math_processor/p4_math_logic.py
@@ -24,3 +24,18 @@ class MathEngine:
     def run_engine(self):
         print(f"{self.CYAN}--- STARTING MATH ENGINE ---{self.RESET}")
         data = self._get_numbers()
+        
+        d_file = open('double.txt', 'w')
+        t_file = open('triple.txt', 'w')
+
+        for n in data:
+            if n % 2 == 0:
+                # Square even numbers
+                d_file.write(str(n ** 2) + "\n")
+            else:
+                # Cube odd numbers
+                t_file.write(str(n ** 3) + "\n")
+
+        d_file.close()
+        t_file.close()
+        print(f"{self.GREEN}✔️ double.txt and triple.txt generated.{self.RESET}")
