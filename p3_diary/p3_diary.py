74518dd552dce2876ad4b3304bf31bf4301d3def
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-04-29 00:35:53 +0800

    Added the strftime for the hours... By this, we have finished the feature of our program where it automates adding the date and time like a log book when it was inputted.

--- p3_diary/p3_diary.py
+++ p3_diary/p3_diary.py
@@ -5,13 +5,16 @@ class LifeVault:
     def __init__(self, target_name):
         self.target_name = target_name
         self.vault = []
+        # Adding the maangas timestamp [MONTH:DAY:YEAR]
         self.today = datetime.now().strftime("%B %d, %Y")
 
     def add_line(self, text):
-        # Basic addition to the buffer
-        self.vault.append(text)
+        # Adding the maangas timestamp [HH:MM:SS]
+        timestamp = datetime.now().strftime("%H:%M:%S")
+        self.vault.append(f"[{timestamp}] {text}")
 
     def finalize(self):
-        # Write the buffer to the file
         with open(self.target_name, "a") as f:
+            # Adds a visual header for the date
+            f.write(f"\n--- LOG ENTRY: {self.today} ---\n")
             f.write("\n".join(self.vault) + "\n")
