d20589feeb9ae40f12408a99adc139912057052c
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-04-29 01:52:44 +0800

    cleaned the code spaces, and structure

--- p4_math_processor/p4_math_processor.py
+++ p4_math_processor/p4_math_processor.py
@@ -1,14 +1,10 @@
-# app.py
 from p4_math_logic import MathEngine
 
-
 def main():
-    # Pass the filename here instead of inside the class
     processor = MathEngine('integers.txt')
 
     processor.run_engine()
     processor.print_summary()
-
 
 if __name__ == "__main__":
     main()
