c2b244777a092aa989024df698886a5253658ca9
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-04-28 23:31:46 +0800

    creation of color variables using ansi colors, also created a variable called analyzer for much more clearer and organized coding when using the module

--- p2_gwa_tracker/gwa_processor.py
+++ p2_gwa_tracker/gwa_processor.py
@@ -1,3 +1,15 @@
 import time
 import os
 from gwa_logic_handling import gwa_logic as gwa_function
+
+def start_application():
+    # Visual UI Colors
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    WHITE = "\033[97m"
+    BOLD = "\033[1m"
+    RESET = "\033[0m"
+
+    # Using 'gwa_function' since it is the class name for the importing.
+    analyzer = gwa_function('students.txt')
