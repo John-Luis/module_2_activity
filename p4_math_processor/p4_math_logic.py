55e694a892c76bb45fa8f2e5ddd018a963fab08a
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-04-29 01:53:29 +0800

    cleaned the code for mas maangas na code

--- p4_math_processor/p4_math_logic.py
+++ p4_math_processor/p4_math_logic.py
@@ -2,7 +2,6 @@ import os
 
 class MathEngine:
     def __init__(self, target_file):
-        # We removed the hardcoded "integers.txt"
         self.target_file = target_file
         self.CYAN = '\033[96m'
         self.GREEN = '\033[92m'
